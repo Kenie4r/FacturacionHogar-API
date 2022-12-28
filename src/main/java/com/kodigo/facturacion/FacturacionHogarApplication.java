16d72d869c5f87ab34f42cb30de4467cd137fc1e
Keneth Nolasco <54483710+Kenie4r@users.noreply.github.com>
2022-12-28 16:33:29 -0600

    Initial commit

---
+++ src/main/java/com/kodigo/facturacion/FacturacionHogarApplication.java
@@ -0,0 +1,13 @@
+package com.kodigo.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionHogarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionHogarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodigo/facturacion/FacturacionHogarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigo.facturacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionHogarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
