2b5a0dba9392acc9cbde42b8613ef3596db78aca
DiegoMolina95 <72415724+DiegoMolina95@users.noreply.github.com>
2023-01-05 15:31:32 -0600

    Create ResourceNotFoundException.java

---
+++ src/main/java/com/kodigo/facturacion/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.kodigo.facturacion.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    @Getter
+    private String resourceName;
+    @Getter
+    private String fieldName;
+    @Getter
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+
+
+}
