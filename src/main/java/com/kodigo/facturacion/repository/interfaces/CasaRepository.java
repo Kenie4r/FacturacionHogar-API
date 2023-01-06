8ad94e191b35294263a27871eb5cff991dce565f
DiegoMolina95 <72415724+DiegoMolina95@users.noreply.github.com>
2023-01-06 13:16:11 -0600

    Create CasaRepository.java

---
+++ src/main/java/com/kodigo/facturacion/repository/interfaces/CasaRepository.java
@@ -0,0 +1,7 @@
+package com.kodigo.facturacion.repository.interfaces;
+
+import com.kodigo.facturacion.persistence.Casa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CasaRepository extends JpaRepository<Casa,Long> {
+}
