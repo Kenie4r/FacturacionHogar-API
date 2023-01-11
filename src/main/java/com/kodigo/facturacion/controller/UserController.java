ae1762c3a1ccfcfb5860b621ae0234657a8c368d
Rafael Mendoza <eespino.mendoza@gmail.com>
2023-01-11 00:25:27 -0600

    BACK END COMPLETO USUARIO

--- src/main/java/com/kodigo/facturacion/controller/UserController.java
+++ src/main/java/com/kodigo/facturacion/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/loggin")
 public class UserController {
@@ -18,6 +20,37 @@ public class UserController {
     public ResponseEntity<Usuario> getUser (@PathVariable("id") String id) throws Exception{
         return new ResponseEntity<Usuario>(user.BuscarUsuarioByName(id), HttpStatus.FOUND);
     }
-    
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Usuario>> getUsers () throws Exception{
+        return new ResponseEntity<List<Usuario>>(user.obtenerUsuarios(), HttpStatus.FOUND);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> saveUsuario(@RequestBody Usuario usuario){
+        String message = user.guardarUsuario(usuario);
+        if(message.equals("Can't save")){
+            throw new RuntimeException("No se ha podido guardar el usuario");
+        }
+        return new ResponseEntity<String>(message, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) throws Exception {
+        return new ResponseEntity<String>(user.eliminarUsuario(id), HttpStatus.OK);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> updateUser(@PathVariable("id") Long id , @RequestBody Usuario usuario){
+        usuario.setIdUsuario(id);
+        String message = user.actualizarUsuario(usuario);
+        if(message.equals("Can't delete it")){
+            throw new RuntimeException("No se ha podido eliminar el usuario");
+        }
+        return new ResponseEntity<String>(message , HttpStatus.OK);
+    }
+
+
+
 
 }
--- src/main/java/com/kodigo/facturacion/service/UserImpl.java
+++ src/main/java/com/kodigo/facturacion/service/UserImpl.java
@@ -6,6 +6,7 @@ import com.kodigo.facturacion.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,5 +29,43 @@ public class UserImpl implements UserService {
         }catch (Exception e){
             return null;
         }
+    }
+
+    @Override
+    public String guardarUsuario(Usuario usuario) {
+        try{
+            userRepository.save(usuario);
+            return "SAVED";
+        }catch (Exception ex){
+            return "Can't save";
+        }
+    }
+
+    @Override
+    public String eliminarUsuario(Long id) {
+        try{
+            Optional<Usuario> usuario = userRepository.findById(id);
+            if(!usuario.isPresent()){
+                return "Can't delete it";
+            }
+            Usuario usuario1 = usuario.get();
+            userRepository.delete(usuario1);
+        }catch (Exception exception){
+            return "Can't delete it";
+        }
+        return "Deleted";
+    }
+
+    @Override
+    public String actualizarUsuario(Usuario usuario) {
+        if(userRepository.save(usuario)!=null){
+            return "Saved";
+        }
+        return "Unsaved";
+    }
+
+    @Override
+    public List<Usuario> obtenerUsuarios() {
+        return userRepository.findAll();
     }
 }
